14161f9422ac95a38b781eb5e8e583f0ffafd726
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-19 22:02:18 -0400

    added master-schedule component

---
+++ src/components/master-schedule.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+import { Container, List } from 'semantic-ui-react'
+
+interface MasterScheduleDate {
+  id: string
+  date: string
+}
+interface MasterScheduleProps {
+  dates: MasterScheduleDate[]
+  selectRehearsal: (id: string) => void
+
+}
+const MasterSchedule = (props: MasterScheduleProps ) => {
+  return (
+    <Container>
+      <List>
+        {props.dates.map(({ date, id }) => {
+          return (
+            <List.Item key={id} content={date} onClick={() => { props.selectRehearsal(id) }} />
+          )
+        })}
+      </List>
+    </Container>
+  )
+}
+
+export default MasterSchedule
